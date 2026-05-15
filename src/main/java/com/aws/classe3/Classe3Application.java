271bd7fa74d4f05bce36caa3c2cd32c0ef621245
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2026-05-15 20:18:09 -0300

    Primeiro commit

---
+++ src/main/java/com/aws/classe3/Classe3Application.java
@@ -0,0 +1,13 @@
+package com.aws.classe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classe3Application.class, args);
+	}
+
+}
